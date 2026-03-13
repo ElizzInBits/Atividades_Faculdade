b98c69902f795dc0c6dcbe8c67c48fae9e52aa56
ElizzInBits <elizabethm174394@gmail.com>
2026-03-13 14:10:16 -0300

    Update main.java

--- ModelosMetodosTecnicasEngenhariaSoftware/SOLID/AveVoador/src/main.java
+++ ModelosMetodosTecnicasEngenhariaSoftware/SOLID/AveVoador/src/main.java
@@ -9,22 +9,22 @@ public class main {
         aguia.voar();
         aguia.emitirSom();
 
-        System.out.println("\n=== Galinha ===");
+        System.out.println("\n=-= Galinha =-=");
         Ave galinha = new Galinha();
         galinha.voar();
         galinha.emitirSom();
 
-        System.out.println("\n=== Avestruz ===");
+        System.out.println("\n=-= Avestruz =-=");
         Ave avestruz = new Avestruz();
         avestruz.voar();
         avestruz.emitirSom();
 
-        System.out.println("\n=== Falcão ===");
+        System.out.println("\n=-= Falcão =-=");
         Ave falcao = new Falcao();
         falcao.voar();
         falcao.emitirSom();
 
-        System.out.println("\n=== Gavião ===");
+        System.out.println("\n=-= Gavião =-=");
         Ave gaviao = new Gaviao();
         gaviao.voar();
         gaviao.emitirSom();
